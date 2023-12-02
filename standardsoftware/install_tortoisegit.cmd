ba2adc5018511201cabf8e698818cce417ad8bc5
Peter Reichart <pr@hochvogel.de>
2023-12-02 22:36:43 +0000

    standardsoftware/install_tortoisegit.cmd hinzugefügt

---
+++ standardsoftware/install_tortoisegit.cmd
@@ -0,0 +1,24 @@
+rem ------------------------------------------------------------
+rem Installationsroutine
+rem ------------------------------------------------------------
+
+rem --- Option: Download in ein temporäres Verzeichnis
+rem --- Kann für z.B. für GLPI abgeschaltet werden
+set SAVEPATH=%SYSTEMDRIVE%\TEMP\install
+MKDIR %SAVEPATH% & CD /D %SAVEPATH%
+
+rem --- Download Basis-URL, Dateiname und TEMP-Verzeichnis
+set BASEURL=https://download.tortoisegit.org/tgit/2.15.0.0
+set PROG=TortoiseGit-2.15.0.0-64bit.msi
+
+rem --- Lade Installationsdatei herunter
+curl -k %BASEURL%/%PROG% -o %SAVEPATH%\%PROG%
+
+rem --- Silent-Installation des Programms:
+%SYSTEMDRIVE%\windows\system32\msiexec /i "%PROG%" /passive /l %SAVEPATH%\%PROG%.log 
+
+rem ---- Beispiele ---------------------------------------------
+rem %PROG% /S
+rem %PROG% /VERYSILENT /NORESTART /ALLUSERS
+rem %PROG% /install /quiet /norestart
+rem %SYSTEMDRIVE%\windows\system32\msiexec /i "%PROG%" /passive /l %SAVEPATH%\%PROG%.log 
