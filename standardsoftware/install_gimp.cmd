37b0500ab8204967316b7415625d61d60dc2998e
Peter Reichart <pr@hochvogel.de>
2023-12-02 22:23:48 +0000

    standardsoftware/install_gimp.cmd aktualisiert

--- standardsoftware/install_gimp.cmd
+++ standardsoftware/install_gimp.cmd
@@ -1,11 +1,22 @@
 rem ------------------------------------------------------------
-rem Installiere Gimp
+rem Installationsroutine
 rem ------------------------------------------------------------
 
-set BASEURL=https://glpi.albrecht-elektrotechnik.local/repo/standardsoftware
+rem --- Download Basis-URL, Dateiname und TEMP-Verzeichnis
+set BASEURL=https://download.gimp.org/gimp/v2.10/windows
 set PROG=gimp-2.10.36-setup.exe
+set SAVEPATH=%SYSTEMDRIVE%\TEMP\install
 
-MKDIR C:\temp\ & CD /D C:\temp
-curl -k %BASEURL%/%PROG% -o c:\temp\%PROG%
+rem --- Option: Download in ein temporäres Verzeichnis
+rem --- Kann für z.B. für GLPI abgeschaltet werden
+MKDIR %SAVEPATH% & CD /D %SAVEPATH%
+curl -k %BASEURL%/%PROG% -o %SAVEPATH%\%PROG%
 
-%PROG% /VERYSILENT /NORESTART /ALLUSERS
+rem --- Silent-Installation des Programms:
+%PROG% /S
+
+rem ---- Beispiele ---------------------------------------------
+rem %PROG% /S
+rem %PROG% /VERYSILENT /NORESTART /ALLUSERS
+rem %PROG% /install /quiet /norestart
+rem %SYSTEMDRIVE%\windows\system32\msiexec /i "%PROG%" /passive /l %SAVEPATH%\%PROG%.log 
