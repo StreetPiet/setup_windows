035707bdbc21969074b4cb5748b4ba91c1f7510e
Peter Reichart <pr@hochvogel.de>
2023-12-02 22:20:33 +0000

    standardsoftware/install_filezilla_client.cmd aktualisiert

--- standardsoftware/install_filezilla_client.cmd
+++ standardsoftware/install_filezilla_client.cmd
@@ -2,12 +2,11 @@ rem ------------------------------------------------------------
 rem Installiere 
 rem ------------------------------------------------------------
 
-set BASEURL=https://glpi.albrecht-elektrotechnik.local/repo/standardsoftware
+set BASEURL=https://dl2.cdn.filezilla-project.org/client
 set PROG=FileZilla_3.66.1_win64-setup.exe
 
 MKDIR C:\temp\ & CD /D C:\temp
-curl -k %BASEURL%/%PROG% -o c:\temp\%PROG%
-
+curl -k "%BASEURL%/%PROG%?h=od6T6ObVh3-eWWW32MTEKg&x=1701558975" -o c:\temp\%PROG%
 
 %PROG% /S /user=all 
 
