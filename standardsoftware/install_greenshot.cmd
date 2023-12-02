aba86c4ce097c759c8ec4b5d425f4976dcd08579
Peter Reichart <pr@hochvogel.de>
2023-12-02 22:26:17 +0000

    standardsoftware/install_greenshot.cmd aktualisiert

--- standardsoftware/install_greenshot.cmd
+++ standardsoftware/install_greenshot.cmd
@@ -2,11 +2,14 @@ rem ------------------------------------------------------------
 rem Installiere Greenshot
 rem ------------------------------------------------------------
 
-set BASEURL=https://glpi.albrecht-elektrotechnik.local/repo/standardsoftware
-set PROG=Greenshot%%201.2.10.6.exe
+set BASEURL=https://github.com/greenshot/greenshot/releases/download/Greenshot-RELEASE-1.2.10.6
+set PROG=Greenshot-INSTALLER-1.2.10.6-RELEASE.exe
 
-MKDIR C:\temp\ & CD /D C:\temp
-curl -k %BASEURL%/%PROG% -o c:\temp\%PROG%
+rem --- Option: Download in ein temporäres Verzeichnis
+rem --- Kann für z.B. für GLPI abgeschaltet werden
+set SAVEPATH=%SYSTEMDRIVE%\TEMP\install
+MKDIR %SAVEPATH% & CD /D %SAVEPATH%
+curl -k %BASEURL%/%PROG% -o %SAVEPATH%\%PROG%
 
 (
 echo [Setup]
