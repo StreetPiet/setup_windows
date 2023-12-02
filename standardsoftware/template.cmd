ececb7fc185d62bfb6ab701a0d8e191601a4ba98
Peter Reichart <pr@hochvogel.de>
2023-12-02 22:26:57 +0000

    standardsoftware/template.cmd aktualisiert

--- standardsoftware/template.cmd
+++ standardsoftware/template.cmd
@@ -5,11 +5,13 @@ rem ------------------------------------------------------------
 rem --- Download Basis-URL, Dateiname und TEMP-Verzeichnis
 set BASEURL=https://glpi.albrecht-elektrotechnik.local/repo/standardsoftware
 set PROG=
-set SAVEPATH=%SYSTEMDRIVE%\TEMP\install
 
 rem --- Option: Download in ein temporäres Verzeichnis
 rem --- Kann für z.B. für GLPI abgeschaltet werden
+set SAVEPATH=%SYSTEMDRIVE%\TEMP\install
 MKDIR %SAVEPATH% & CD /D %SAVEPATH%
+
+rem --- Lade Installationsdatei herunter
 curl -k %BASEURL%/%PROG% -o %SAVEPATH%\%PROG%
 
 rem --- Silent-Installation des Programms:
