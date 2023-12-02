298376a19b91a32fc712a8f9c4a70730d011e33c
Peter Reichart <pr@hochvogel.de>
2023-12-02 22:08:55 +0000

    standardsoftware/install_7zip.cmd aktualisiert

--- standardsoftware/install_7zip.cmd
+++ standardsoftware/install_7zip.cmd
@@ -1,10 +1,22 @@
 rem ------------------------------------------------------------
-rem Installiere 7-Zip
+rem Installationsroutine
 rem ------------------------------------------------------------
 
-set BASEURL=https://glpi.albrecht-elektrotechnik.local/repo/standardsoftware
+rem --- Download Basis-URL, Dateiname und TEMP-Verzeichnis
+set BASEURL=https://7-zip.org/a
 set PROG=7z2301-x64.msi
+set SAVEPATH=%SYSTEMDRIVE%\TEMP\install
 
-MKDIR C:\temp\ & CD /D C:\temp
-curl -k %BASEURL%/%PROG% -o c:\temp\%PROG%
-c:\windows\system32\msiexec /i "%PROG%" /passive /l c:\temp\%PROG%.log 
+rem --- Option: Download in ein temporäres Verzeichnis
+rem --- Kann für z.B. für GLPI abgeschaltet werden
+MKDIR %SAVEPATH% & CD /D %SAVEPATH%
+curl -k %BASEURL%/%PROG% -o %SAVEPATH%\%PROG%
+
+rem --- Silent-Installation des Programms:
+%SYSTEMDRIVE%\windows\system32\msiexec /i "%PROG%" /passive /l %SAVEPATH%\%PROG%.log 
+
+rem ---- Beispiele ---------------------------------------------
+rem %PROG% /S
+rem %PROG% /VERYSILENT /NORESTART /ALLUSERS
+rem %PROG% /install /quiet /norestart
+rem c:\windows\system32\msiexec /i "%PROG%" /passive /l %SAVEPATH%\%PROG%.log 
