4922daab1472c2ccd4729ea7e53e0c8f9955b69b
Peter Reichart <pr@hochvogel.de>
2023-12-03 01:36:18 +0000

    win10_initial_install.cmd aktualisiert

--- win10_initial_install.cmd
+++ win10_initial_install.cmd
@@ -11,7 +11,7 @@ curl -k -L %GITURL%/skripte/script_win10_std_einstellungen.cmd -o script.cmd & c
 curl -k -L %GITURL%/skripte/script_onedrive_deinstallieren.cmd -o script.cmd & call script.cmd
 
 rem --- Essentielle Software
-curl -k -L https://support.ostrachhelp.de/OstrachNET/setup_windows/raw/branch/main/essentiell/install_vc_redist_runtime.cmd -o script.cmd & script.cmd
+curl -k -L https://support.ostrachhelp.de/OstrachNET/setup_windows/raw/branch/main/essentiell/install_vc_redist_runtime.cmd -o script.cmd & call script.cmd
 set GITURL=https://support.ostrachhelp.de/OstrachNET/setup_windows/raw/branch/main/standardsoftware
 curl -k -L %GITURL%/install_7zip.cmd -o script.cmd & call script.cmd
 curl -k -L %GITURL%/install_firefox.cmd -o script.cmd & call script.cmd
