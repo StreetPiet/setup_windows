aef50151b730522758d17db3b08eff99ae572cb8
Peter Reichart <pr@hochvogel.de>
2023-12-03 15:23:50 +0000

    admin/install_tightvnc.cmd hinzugefügt

---
+++ admin/install_tightvnc.cmd
@@ -0,0 +1,19 @@
+rem ------------------------------------------------------------
+rem Installationsroutine
+rem ------------------------------------------------------------
+
+rem --- Option: Download in ein temporäres Verzeichnis
+rem --- Kann für z.B. für GLPI abgeschaltet werden
+set SAVEPATH=%SYSTEMDRIVE%\TEMP\install
+MKDIR %SAVEPATH% & CD /D %SAVEPATH%
+
+rem --- Download Basis-URL, Dateiname und TEMP-Verzeichnis
+set BASEURL=https://www.tightvnc.com/download/2.8.81
+set PROG=tightvnc-2.8.81-gpl-setup-64bit.msi
+
+rem --- Lade Installationsdatei herunter
+curl -k -L %BASEURL%/%PROG% -o %SAVEPATH%\%PROG%
+
+rem --- Silent-Installation des Programms:
+%SYSTEMDRIVE%\windows\system32\msiexec /i "%PROG%" /passive /l %SAVEPATH%\%PROG%.log 
+
