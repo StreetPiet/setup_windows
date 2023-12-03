042460f4f4305d84fe2f83aafecf1ac42d2527d6
Peter Reichart <pr@hochvogel.de>
2023-12-03 13:39:13 +0100

    ...

--- skripte/script_win11_std_einstellungen.cmd
+++ skripte/script_win11_std_einstellungen.cmd
@@ -17,10 +17,10 @@ REG DELETE "HKEY_CURRENT_USER\Software\Classes\CLSID\{018D5C66-4533-4307-9B53-22
 REG DELETE "HKEY_CURRENT_USER\Software\Classes\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}\ShellFolder" /f 
 REG DELETE "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v "OneDrive" /f
 
-rem ---- Dateinamenerweiterungen einschalten
+rem ---- Dateinamenerweiterungen einschalten OK!
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 0 /f
 
-rem ---- Energieeinstellungen
+rem ---- Energieeinstellungen OK!
 powercfg /change standby-timeout-ac 0
 powercfg /change standby-timeout-dc 10
 powercfg /change hibernate-timeout-ac 0
@@ -29,39 +29,36 @@ powercfg /change monitor-timeout-ac 30
 powercfg /change monitor-timeout-dc 10
 powercfg /change disk-timeout-ac 0
 powercfg /change disk-timeout-dc 20
-rem ---- FastBoot ausschalten
+
+rem ---- FastBoot ausschalten OK!
 reg add "HKLM\System\CurrentControlSet\Control\Session Manager\Power" /v HiberbootEnabled /t REG_DWORD /d 0 /f
 
-rem ---- Energiesparplan auf Höchstleistung setzen
+rem ---- Energiesparplan auf Höchstleistung setzen OK!
 powercfg /SETACTIVE /SCHEME_MIN
 
-rem ---- Beim Zuklappen des Bildschirms nichts unternehmen
+rem ---- Beim Zuklappen des Bildschirms nichts unternehmen OK!
 powercfg /SETDCVALUEINDEX SCHEME_MIN SUB_BUTTONS LIDACTION 0
 powercfg /SETACVALUEINDEX SCHEME_MIN SUB_BUTTONS LIDACTION 0
 
-rem ---- Beim drücken des Netzschalters herunterfahren
+rem ---- Beim drücken des Netzschalters herunterfahren OK!
 powercfg /SETDCVALUEINDEX SCHEME_MIN SUB_BUTTONS PBUTTONACTION 3
 powercfg /SETACVALUEINDEX SCHEME_MIN SUB_BUTTONS PBUTTONACTION 3
 
-rem ---- Beim drücken der Energiespartaste energie sparen
-powercfg /SETDCVALUEINDEX SCHEME_MIN SUB_BUTTONS SleepButtonAction 1
-powercfg /SETACVALUEINDEX SCHEME_MIN SUB_BUTTONS SleepButtonAction 1
-
-rem ---- Cortana & Taskansicht ausschalten
+rem ---- Cortana & Taskansicht ausschalten OK!
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Search" /v SearchboxTaskbarMode /t REG_DWORD /d 0 /f
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowCortanaButton /t REG_DWORD /d 0 /f
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowTaskViewButton /t REG_DWORD /d 0 /f
 
-rem ---- Zeige alle Symbole in der Taskleiste
+rem ---- Zeige alle Symbole in der Taskleiste OK!
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer" /v EnableAutoTray /t REG_DWORD /d 0 /f
 
-rem ---- Live Kacheln im Startmenue abschalten
+rem ---- Live Kacheln im Startmenue abschalten OK!
 reg add "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\CurrentVersion\PushNotifications" /v NoTileApplicationNotification /t REG_DWORD /d 1 /f
 
-rem ---- 3D-Objects Ordner im Explorer ausblenden
+rem ---- 3D-Objects Ordner im Explorer ausblenden - REG nicht gefunden
 reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace" /v {0DB7E03F-FC29-4DC6-9020-FF41B59E513A} /f
 
-rem ---- Schalte Newsfeed in der Taskleiste bei Windows 10 21H1 ab
+rem ---- Schalte Newsfeed in der Taskleiste  ab OK!
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Feeds" /v ShellFeedsTaskbarViewMode /t REG_DWORD /d 2 /f
 
 rem ---- Logoff!
