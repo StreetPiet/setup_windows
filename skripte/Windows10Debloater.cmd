5482ae562bf1d8294c2088e72593cda8dd27bf94
Peter Reichart <pr@hochvogel.de>
2023-12-03 03:16:30 +0100

    inital commit

---
+++ skripte/Windows10Debloater.cmd
@@ -0,0 +1,14 @@
+rem -----------------------------------------------------------------
+rem Installiere Windows10Debloater
+rem -----------------------------------------------------------------
+
+rem --- Option: Download in ein temporäres Verzeichnis
+rem --- Kann für z.B. für GLPI abgeschaltet werden
+set SAVEPATH=%SYSTEMDRIVE%\TEMP\install
+MKDIR %SAVEPATH% & CD /D %SAVEPATH%
+
+rem --- Lade Debloater .ZIP
+set BASEURL=https://support.ostrachhelp.de/OstrachNET/setup_windows/raw/branch/main/skripte
+set PROG=Windows10Debloater-master.zip
+curl -k -L %BASEURL%/%PROG% -o %PROG%
+powershell.exe -command "Expand-Archive -Path %PROG% -DestinationPath . "
