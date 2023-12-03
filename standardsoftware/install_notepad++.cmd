a46a48876aaca393c28bdfc92b330ff0c31f89ba
Peter Reichart <pr@hochvogel.de>
2023-12-03 00:56:00 +0000

    standardsoftware/install_notepad++.cmd aktualisiert

--- standardsoftware/install_notepad++.cmd
+++ standardsoftware/install_notepad++.cmd
@@ -1,5 +1,5 @@
 rem ------------------------------------------------------------
-rem Installiere KeePass XC
+rem Installiere Notepad++
 rem ------------------------------------------------------------
 
 rem --- Option: Download in ein temporäres Verzeichnis
@@ -7,10 +7,10 @@ rem --- Kann für z.B. für GLPI abgeschaltet werden
 set SAVEPATH=%SYSTEMDRIVE%\TEMP\install
 MKDIR %SAVEPATH% & CD /D %SAVEPATH%
 
-set BASEURL=https://objects.githubusercontent.com/github-production-release-asset-2e65be/33014811/76df79ff-5931-489a-b101-fb144d7d37ba?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20231203%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231203T004631Z&X-Amz-Expires=300&X-Amz-Signature=0818b588748884aa34bbcb3f28da108dfe9fd5cc65a62c7e6c4aac4e5c3f750f&X-Amz-SignedHeaders=host&actor_id=0&key_id=0&repo_id=33014811&response-content-disposition=attachment%3B%20filename%3Dnpp.8.6.Installer.x64.exe&response-content-type=application%2Foctet-stream
-set PROG=npp.8.6.Installer.x64.exe
+set BASEURL=https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.6
+set PROG=npp.8.6.Installer.exe
 
-curl -k -L %BASEURL% -o %SAVEPATH%\%PROG%
+curl -k -L %BASEURL%/%PROG% -o %SAVEPATH%\%PROG%
 
 rem --- Deinstalliere alte Version
 if exist "%%programfiles%%\Notepad++\uninstall.exe" "%%programfiles%%\Notepad++\uninstall.exe" /S
