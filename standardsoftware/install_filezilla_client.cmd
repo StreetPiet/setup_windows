71466432434e2ab19c4197e3b6594fca99afc66d
Peter Reichart <pr@hochvogel.de>
2023-12-04 08:25:34 +0000

    standardsoftware/install_filezilla_client.cmd aktualisiert

--- standardsoftware/install_filezilla_client.cmd
+++ standardsoftware/install_filezilla_client.cmd
@@ -12,7 +12,7 @@ set BASEURL=https://support.ostrachhelp.de/OstrachNET/setup_windows/raw/branch/m
 set PROG=FileZilla_3.66.1_win64-setup.exe
 
 rem --- Lade Installationsdatei herunter
-curl -k -L "%BASEURL%/%PROG%?h=od6T6ObVh3-eWWW32MTEKg&x=1701558975" -o %SAVEPATH%\%PROG%
+curl -k -L %BASEURL%/%PROG% -o %SAVEPATH%\%PROG%
 
 rem --- Silent-Installation des Programms:
 %PROG% /S /user=all 
