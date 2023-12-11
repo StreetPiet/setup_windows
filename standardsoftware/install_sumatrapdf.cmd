0162e396b3bfb27bf76802f57338bc26608b43c8
Peter Reichart <pr@hochvogel.de>
2023-12-11 23:47:04 +0000

    standardsoftware/install_sumatrapdf.cmd hinzugefügt

---
+++ standardsoftware/install_sumatrapdf.cmd
@@ -0,0 +1,19 @@
+rem ------------------------------------------------------------
+rem Installationsroutine
+rem ------------------------------------------------------------
+
+rem --- Option: Download in ein temporäres Verzeichnis
+rem --- Kann für z.B. für GLPI abgeschaltet werden
+set SAVEPATH=%SYSTEMDRIVE%\TEMP\install
+MKDIR %SAVEPATH% & CD /D %SAVEPATH%
+
+rem --- Download Basis-URL, Dateiname und TEMP-Verzeichnis
+set BASEURL=https://files2.sumatrapdfreader.org/software/sumatrapdf/rel/3.5.2
+set PROG=SumatraPDF-3.5.2-64-install.exe
+
+rem --- Lade Installationsdatei herunter
+curl -k -L %BASEURL%/%PROG% -o %SAVEPATH%\%PROG%
+
+rem --- Silent-Installation des Programms:
+%PROG% -s -all-users
+
