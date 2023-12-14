bd8bf992a0687d1e716059fd0d17da867eddef7e
Peter Reichart <pr@hochvogel.de>
2023-12-14 10:58:52 +0000

    packs/media_pack.cmd hinzugefügt

---
+++ packs/media_pack.cmd
@@ -0,0 +1,16 @@
+rem -----------------------------------------------------------------------
+rem Media-Pack
+rem 
+rem curl -k -L https://support.ostrachhelp.de/OstrachNET/setup_windows/raw/branch/main/win10_initial_install.cmd -o start.cmd
+rem
+rem -----------------------------------------------------------------------
+
+set SAVEPATH=%SYSTEMDRIVE%\TEMP\install
+MKDIR %SAVEPATH% & CD /D %SAVEPATH%
+
+rem --- Ordner: Standardsoftware
+set STDURL=https://support.ostrachhelp.de/OstrachNET/setup_windows/raw/branch/main/standardsoftware
+curl -kLO %STDURL%/install_audacity.cmd & call install_audacity.cmd
+curl -kLO %STDURL%/install_vlc.cmd & call install_vlc.cmd
+curl -kLO %STDURL%/install_gimp.cmd & call install_gimp.cmd
+
