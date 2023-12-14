bd93ba1ee9f8097c9173b95e80802b8412bb9808
Peter Reichart <pr@hochvogel.de>
2023-12-14 11:07:33 +0000

    packs/essentials.cmd hinzugefügt

---
+++ packs/essentials.cmd
@@ -0,0 +1,15 @@
+rem -----------------------------------------------------------------------
+rem Essentials
+rem
+rem Must-have Programme für Jedermann
+rem -----------------------------------------------------------------------
+
+set SAVEPATH=%SYSTEMDRIVE%\TEMP\install
+MKDIR %SAVEPATH% & CD /D %SAVEPATH%
+
+rem --- Ordner: Standardsoftware
+set STDURL=https://support.ostrachhelp.de/OstrachNET/setup_windows/raw/branch/main/standardsoftware
+curl -kLO %STDURL%/install_7zip.cmd & call install_7zip.cmd
+curl -kLO %STDURL%/install_firefox.cmd & call install_firefox.cmd
+curl -kLO %STDURL%/install_keepass.cmd & call install_keepass.cmd
+curl -kLO %STDURL%/install_notepad++.cmd & call install_notepad++.cmd
