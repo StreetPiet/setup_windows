dd9c84f1244aee4d9f441194904e88af0efc2f80
Peter Reichart <pr@hochvogel.de>
2023-12-14 11:13:49 +0000

    win10_initial_install.cmd aktualisiert

--- win10_initial_install.cmd
+++ win10_initial_install.cmd
@@ -14,9 +14,5 @@ curl -k -L %GITURL%/skripte/script_win10_std_einstellungen.cmd -o script.cmd & c
 curl -k -L %GITURL%/skripte/script_onedrive_deinstallieren.cmd -o script.cmd & call script.cmd
 curl -k -L %GITURL%/skripte/Windows10Debloater.cmd -o script.cmd & call script.cmd
 
-rem --- Essentielle Software
-curl -k -L https://support.ostrachhelp.de/OstrachNET/setup_windows/raw/branch/main/essentiell/install_vc_redist_runtime.cmd -o script.cmd & call script.cmd
-set GITURL=https://support.ostrachhelp.de/OstrachNET/setup_windows/raw/branch/main/standardsoftware
-curl -k -L %GITURL%/install_7zip.cmd -o script.cmd & call script.cmd
-curl -k -L %GITURL%/install_firefox.cmd -o script.cmd & call script.cmd
-curl -k -L "%GITURL%/install_notepad++.cmd" -o script.cmd & call script.cmd
+rem --- Software-Pack: Essentials
+curl -kLO https://support.ostrachhelp.de/OstrachNET/setup_windows/src/branch/main/packs/essentials.cmd & call essentials.cmd
