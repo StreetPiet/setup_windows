2f15c6b114387fab40e38307a1162a385a4ce6fb
Peter Reichart <pr@hochvogel.de>
2023-12-14 11:03:02 +0000

    packs/office_pack.cmd hinzugefügt

---
+++ packs/office_pack.cmd
@@ -0,0 +1,15 @@
+rem -----------------------------------------------------------------------
+rem Office-Pack
+rem -----------------------------------------------------------------------
+
+set SAVEPATH=%SYSTEMDRIVE%\TEMP\install
+MKDIR %SAVEPATH% & CD /D %SAVEPATH%
+
+rem --- Ordner: Standardsoftware
+set STDURL=https://support.ostrachhelp.de/OstrachNET/setup_windows/raw/branch/main/standardsoftware
+curl -kLO %STDURL%/install_greenshot.cmd & call install_greenshot.cmd
+curl -kLO %STDURL%/install_sumatrapdf.cmd & call install_sumatrapdf.cmd
+curl -kLO %STDURL%/install_pdf24.cmd & call install_pdf24.cmd
+curl -kLO %STDURL%/install_libreoffice_x64.cmd & call install_libreoffice_x64.cmd
+
+
