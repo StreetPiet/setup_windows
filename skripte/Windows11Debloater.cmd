50e4d9b9252aa761dea1ffb1631a1c0203df57eb
Peter Reichart <pr@hochvogel.de>
2023-12-15 11:45:51 +0000

    skripte/Windows11Debloater.cmd hinzugefügt

---
+++ skripte/Windows11Debloater.cmd
@@ -0,0 +1,16 @@
+rem -----------------------------------------------------------------
+rem Installiere Windows11Debloater
+rem -----------------------------------------------------------------
+
+rem --- Option: Download in ein temporäres Verzeichnis
+rem --- Kann für z.B. für GLPI abgeschaltet werden
+set SAVEPATH=%SYSTEMDRIVE%\TEMP\install
+MKDIR %SAVEPATH% & CD /D %SAVEPATH%
+
+rem --- Lade Debloater .ZIP
+set BASEURL=https://support.ostrachhelp.de/OstrachNET/setup_windows/raw/branch/main/skripte
+set PROG=Win11Debloat-master.zip
+curl -kLO %BASEURL%/%PROG%
+powershell.exe -command "Expand-Archive -Force -Path %PROG% -DestinationPath . "
+CD Win11Debloat-master
+PowerShell -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0Win11Debloat.ps1""' -Verb RunAs}"
