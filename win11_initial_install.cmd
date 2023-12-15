8731ebda52b4902991c9fa5037379e21f8edd3b8
Peter Reichart <pr@hochvogel.de>
2023-12-15 12:19:47 +0000

    win11_initial_install.cmd aktualisiert

--- win11_initial_install.cmd
+++ win11_initial_install.cmd
@@ -9,6 +9,7 @@ rem --- Windows Tweaks
 set GITURL=https://support.ostrachhelp.de/OstrachNET/setup_windows/raw/branch/main
 curl -kLO %GITURL%/skripte/script_win10_std_einstellungen.cmd & call script_win11_std_einstellungen.cmd
 curl -kLO %GITURL%/skripte/script_onedrive_deinstallieren.cmd & call script_onedrive_deinstallieren.cmd
+curl -kLO %GITURL%/skripte/Windows11Debloater.cmd             & call Windows11Debloater.cmd
 
 rem --- Software-Pack: Essentials
 curl -kLO %GITURL%/packs/essentials.cmd & call essentials.cmd
