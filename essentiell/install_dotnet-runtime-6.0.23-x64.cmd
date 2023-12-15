c85f2912b04a75765155a30c22182e28fcade9aa
Peter Reichart <pr@hochvogel.de>
2023-12-15 14:29:46 +0000

    essentiell/install_dotnet-runtime-6.0.23-x64.cmd aktualisiert

--- essentiell/install_dotnet-runtime-6.0.23-x64.cmd
+++ essentiell/install_dotnet-runtime-6.0.23-x64.cmd
@@ -10,6 +10,6 @@ MKDIR %SAVEPATH% & CD /D %SAVEPATH%
 set BASEURL=https://support.ostrachhelp.de/OstrachNET/setup_windows/raw/branch/main/binaries
 set PROG=dotnet-runtime-6.0.23-win-x64.exe
 rem --- Lade Installationsdatei herunter
-curl -k %BASEURL%/%PROG% -o %SAVEPATH%\%PROG%
+curl -kLO %BASEURL%/%PROG%
 rem --- Silent-Installation des Programms:
 %PROG% /install /quiet /norestart
