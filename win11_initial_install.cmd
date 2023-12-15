1c70fb17db033a10b400794e99ad0457370baa59
Peter Reichart <pr@hochvogel.de>
2023-12-15 08:47:49 +0000

    win11_initial_install.cmd hinzugefügt

---
+++ win11_initial_install.cmd
@@ -0,0 +1,14 @@
+rem -----------------------------------------------------------------------
+rem Standardinstallation für ein frisch installiertes Windows 11 rem 
+rem -----------------------------------------------------------------------
+
+set SAVEPATH=%SYSTEMDRIVE%\TEMP\install
+MKDIR %SAVEPATH% & CD /D %SAVEPATH%
+
+rem --- Windows Tweaks
+set GITURL=https://support.ostrachhelp.de/OstrachNET/setup_windows/raw/branch/main
+curl -kLO %GITURL%/skripte/script_win10_std_einstellungen.cmd & call script_win11_std_einstellungen.cmd
+curl -kLO %GITURL%/skripte/script_onedrive_deinstallieren.cmd & call script_onedrive_deinstallieren.cmd
+
+rem --- Software-Pack: Essentials
+curl -kLO %GITURL%/packs/essentials.cmd & call essentials.cmd
