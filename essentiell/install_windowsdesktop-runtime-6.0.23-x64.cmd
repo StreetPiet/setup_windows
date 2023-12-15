b79338ce300eaa0ad2fe0c4db7a5440060b3ae88
Peter Reichart <pr@hochvogel.de>
2023-12-15 14:30:01 +0000

    essentiell/install_windowsdesktop-runtime-6.0.23-x64.cmd aktualisiert

--- essentiell/install_windowsdesktop-runtime-6.0.23-x64.cmd
+++ essentiell/install_windowsdesktop-runtime-6.0.23-x64.cmd
@@ -10,6 +10,6 @@ MKDIR %SAVEPATH% & CD /D %SAVEPATH%
 set BASEURL=https://support.ostrachhelp.de/OstrachNET/setup_windows/raw/branch/main/binaries
 set PROG=windowsdesktop-runtime-6.0.23-win-x64.exe
 rem --- Lade Installationsdatei herunter
-curl -k %BASEURL%/%PROG% -o %SAVEPATH%\%PROG%
+curl -kL %BASEURL%/%PROG% -o %SAVEPATH%\%PROG%
 rem --- Silent-Installation des Programms:
 %PROG% /install /quiet /norestart
