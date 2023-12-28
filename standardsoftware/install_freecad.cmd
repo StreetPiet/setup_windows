2100316ac7c2f31f1a67cac77320181b16326893
Peter Reichart <pr@hochvogel.de>
2023-12-28 14:42:44 +0000

    standardsoftware/install_freecad.cmd hinzugefügt

---
+++ standardsoftware/install_freecad.cmd
@@ -0,0 +1,31 @@
+rem ------------------------------------------------------------
+rem Installationsroutine
+rem ------------------------------------------------------------
+
+rem --- Option: Download in ein temporäres Verzeichnis
+rem --- Kann für z.B. für GLPI abgeschaltet werden
+set SAVEPATH=%SYSTEMDRIVE%\TEMP\install
+MKDIR %SAVEPATH% & CD /D %SAVEPATH%
+
+rem --- Download Basis-URL, Dateiname und TEMP-Verzeichnis
+set VERSION=0.21.1
+set BASEURL=https://github.com/FreeCAD/FreeCAD/releases/download/%VERSION%
+set PROG=FreeCAD-%VERSION%-WIN-x64-installer-1.exe
+
+rem --- Lade Installationsdatei herunter
+curl -k -L %BASEURL%/%PROG% -o %SAVEPATH%\%PROG%
+
+rem --- Silent-Installation des Programms:
+%PROG% /S
+
+
+rem --- Installiere C++ Redistibutable
+rem set BASEURL=https://support.ostrachhelp.de/OstrachNET/setup_windows/raw/branch/main/essentiell
+rem set PROG=VC_redist.x64.exe
+rem curl -k -L %BASEURL%/%PROG% -o %SAVEPATH%\%PROG% & %PROG% /Q
+
+rem ---- Beispiele ---------------------------------------------
+rem %PROG% /S
+rem %PROG% /VERYSILENT /NORESTART /ALLUSERS
+rem %PROG% /install /quiet /norestart
+rem %SYSTEMDRIVE%\windows\system32\msiexec /i "%PROG%" /passive /l %SAVEPATH%\%PROG%.log 
