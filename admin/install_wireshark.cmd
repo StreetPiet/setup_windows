616ebc9123cb6bc1447e53a8bc9f4df4ed2ce8e2
Peter Reichart <pr@hochvogel.de>
2024-01-07 09:31:18 +0000

    admin/install_wireshark.cmd aktualisiert

--- admin/install_wireshark.cmd
+++ admin/install_wireshark.cmd
@@ -7,6 +7,9 @@ rem --- Kann für z.B. für GLPI abgeschaltet werden
 set SAVEPATH=%SYSTEMDRIVE%\TEMP\install
 MKDIR %SAVEPATH% & CD /D %SAVEPATH%
 
+rem --- npcap herunterladen (muss manuell installiert werden)
+curl -kLO https://npcap.com/dist/npcap-1.78.exe
+
 rem --- Download Basis-URL, Dateiname und TEMP-Verzeichnis
 set BASEURL=https://2.na.dl.wireshark.org/win64
 set PROG=Wireshark-4.2.2-x64.exe
