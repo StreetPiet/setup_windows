7bfa44d0c95c0682c7ab6bd837c72e30c268ff31
Peter Reichart <pr@hochvogel.de>
2024-01-15 19:55:29 +0000

    standardsoftware/install_microsip.cmd aktualisiert

--- standardsoftware/install_microsip.cmd
+++ standardsoftware/install_microsip.cmd
@@ -14,25 +14,21 @@ set PROG=MicroSIP-3.21.3.zip
 rem --- Lade Installationsdatei herunter
 curl -k -L %BASEURL%/%PROG% -o %SAVEPATH%\%PROG%
 
-rem --- Unzipper erstellen
-Call :UnZipFile "%SAVEPATH%\EXT\" "%PROG%"
-GOTO INSTALL
-
-:UnZipFile <ExtractTo> <newzipfile>
-set vbs="%SAVEPATH%\unzipper.vbs"
-if exist %vbs% del /f /q %vbs%
->%vbs%  echo Set fso = CreateObject("Scripting.FileSystemObject")
->>%vbs% echo If NOT fso.FolderExists(%1) Then
->>%vbs% echo fso.CreateFolder(%1)
->>%vbs% echo End If
->>%vbs% echo set objShell = CreateObject("Shell.Application")
->>%vbs% echo set FilesInZip=objShell.NameSpace(%2).items
->>%vbs% echo objShell.NameSpace(%1).CopyHere(FilesInZip)
->>%vbs% echo Set fso = Nothing
->>%vbs% echo Set objShell = Nothing
-cscript //nologo %vbs%
-if exist %vbs% del /f /q %vbs%
+rem --- Unzipping....
+powershell.exe "& {Expand-Archive %PROG% -DestinationPath %PROG%EXT}"
 
 rem --- Silent-Installation des Programms:
-:INSTALL
+MKDIR "%Programfiles(x86)%\MicroSIP"
+COPY %PROG%EXT\*.* "%Programfiles(x86)%\MicroSIP\"
 
+rem --- Verknüpfung
+set VBS=link.vbs
+echo Set oWS = WScript.CreateObject("WScript.Shell")                >%VBS%
+echo sLinkFile = "C:\Users\Public\Desktop\MicroSIP.lnk"             >>%VBS%
+echo Set oLink = oWS.CreateShortcut(sLinkFile)                      >>%VBS%
+echo oLink.TargetPath = "%Programfiles(x86)%\MicroSIP\microsip.exe" >>%VBS%
+echo oLink.Save                                                     >>%VBS%
+
+cscript /nologo %VBS%
+
+rem MKLINK C:\Users\Public\Desktop\MicroSIP "%Programfiles(x86)%\MicroSIP\microsip.exe"
