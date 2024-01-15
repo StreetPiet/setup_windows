32f73bde9fbad855d1d713c9cf1989fbc983b66c
Peter Reichart <pr@hochvogel.de>
2024-01-15 19:57:09 +0000

    packs/office_pack.cmd aktualisiert

--- packs/office_pack.cmd
+++ packs/office_pack.cmd
@@ -11,5 +11,6 @@ curl -kLO %STDURL%/install_greenshot.cmd       & call install_greenshot.cmd
 curl -kLO %STDURL%/install_sumatrapdf.cmd      & call install_sumatrapdf.cmd
 curl -kLO %STDURL%/install_pdf24.cmd           & call install_pdf24.cmd
 curl -kLO %STDURL%/install_libreoffice_x64.cmd & call install_libreoffice_x64.cmd
+curl -kLO %STDURL%/install_microsip.cmd        & call install_microsip.cmd
 
 
