868643257fa052ed6eb70b2a54b9c4c5b27d6ea3
Peter Reichart <pr@hochvogel.de>
2024-01-15 18:59:03 +0000

    standardsoftware/install_microsip.cmd aktualisiert

--- standardsoftware/install_microsip.cmd
+++ standardsoftware/install_microsip.cmd
@@ -14,6 +14,24 @@ set PROG=MicroSIP-3.21.3.zip
 rem --- Lade Installationsdatei herunter
 curl -k -L %BASEURL%/%PROG% -o %SAVEPATH%\%PROG%
 
+rem --- Unzipper erstellen
+Call :UnZipFile "EXT%PROG%" "%PROG%"
+
+:UnZipFile <ExtractTo> <newzipfile>
+set vbs="%SAVEPATH%\unzipper.vbs"
+if exist %vbs% del /f /q %vbs%
+>%vbs%  echo Set fso = CreateObject("Scripting.FileSystemObject")
+>>%vbs% echo If NOT fso.FolderExists(%1) Then
+>>%vbs% echo fso.CreateFolder(%1)
+>>%vbs% echo End If
+>>%vbs% echo set objShell = CreateObject("Shell.Application")
+>>%vbs% echo set FilesInZip=objShell.NameSpace(%2).items
+>>%vbs% echo objShell.NameSpace(%1).CopyHere(FilesInZip)
+>>%vbs% echo Set fso = Nothing
+>>%vbs% echo Set objShell = Nothing
+cscript //nologo %vbs%
+if exist %vbs% del /f /q %vbs%
+
 rem --- Silent-Installation des Programms:
 
 
