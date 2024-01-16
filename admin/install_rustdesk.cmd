246687a37f45761a65b31b6b23bc6b34639e47d0
Peter Reichart <pr@hochvogel.de>
2024-01-16 16:23:00 +0000

    admin/install_rustdesk.cmd hinzugefügt

---
+++ admin/install_rustdesk.cmd
@@ -0,0 +1,19 @@
+rem ------------------------------------------------------------
+rem Installationsroutine
+rem ------------------------------------------------------------
+
+rem --- Option: Download in ein temporäres Verzeichnis
+rem --- Kann für z.B. für GLPI abgeschaltet werden
+set SAVEPATH=%SYSTEMDRIVE%\TEMP\install
+MKDIR %SAVEPATH% & CD /D %SAVEPATH%
+
+rem --- Download Basis-URL, Dateiname und TEMP-Verzeichnis
+set BASEURL=https://support.ostrachhelp.de/OstrachNET/setup_windows/raw/branch/main/binaries
+set PROG=rustdesk-1.2.3-x86_64.exe
+
+rem --- Lade Installationsdatei herunter
+curl -k -L %BASEURL%/%PROG% -o %SAVEPATH%\%PROG%
+
+rem --- Silent-Installation des Programms:
+%PROG% --silent-install
+
