315e72d15477620673b8d7d21ed9a0f0cd37aef5
Peter Reichart <pr@hochvogel.de>
2024-01-18 08:37:57 +0000

    essentiell/install_backuptools.cmd aktualisiert

--- essentiell/install_backuptools.cmd
+++ essentiell/install_backuptools.cmd
@@ -10,9 +10,11 @@ MKDIR %SAVEPATH% & CD /D %SAVEPATH%
 set BASEURL=https://support.ostrachhelp.de/OstrachNET/setup_windows/raw/branch/main/binaries
 set PROG=Disk2vhd.zip
 curl -kLO %BASEURL%/%PROG%
+powershell.exe "& {Expand-Archive %PROG% -DestinationPath .}"
 
 set BASEURL=https://support.ostrachhelp.de/OstrachNET/setup_windows/raw/branch/main/binaries
 set PROG=Double%%20Driver.zip
 curl -kLO %BASEURL%/%PROG%
+powershell.exe "& {Expand-Archive %PROG% -DestinationPath .}"
 
 
