53f02ad6a0fdc0aeae405d785bd348ef6d3e4b41
Peter Reichart <pr@hochvogel.de>
2024-03-26 12:09:43 +0000

    admin/deploy_rustdesk_ostrachhelp.cmd aktualisiert

--- admin/deploy_rustdesk_ostrachhelp.cmd
+++ admin/deploy_rustdesk_ostrachhelp.cmd
@@ -19,8 +19,9 @@ REM ############################### Please Do Not Edit Below This Line #########
 if not exist C:\Temp\ md C:\Temp\
 cd C:\Temp\
 
-curl -L "https://github.com/rustdesk/rustdesk/releases/download/1.2.3/rustdesk-1.2.3-x86_64.exe" -o rustdesk.exe
-
+RUSTVER=1.2.3-1
+curl -L "https://github.com/rustdesk/rustdesk/releases/download/%RUSTVER%/rustdesk-%RUSTVER%-x86_64.exe" -o rustdesk.exe
+         
 rustdesk.exe --silent-install
 timeout /t 20
 
