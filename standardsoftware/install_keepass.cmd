8b365eb02929bde25b0e3f8fc50bb7593f149778
Peter Reichart <pr@hochvogel.de>
2024-03-28 19:25:26 +0000

    standardsoftware/install_keepass.cmd aktualisiert

--- standardsoftware/install_keepass.cmd
+++ standardsoftware/install_keepass.cmd
@@ -13,16 +13,13 @@ set PROG=VC_redist.x64.exe
 curl -k -L %BASEURL%/%PROG% -o %SAVEPATH%\%PROG% & %PROG% /Q
 
 rem --- Download Basis-URL, Dateiname und TEMP-Verzeichnis
-set BASEURL=https://github.com/keepassxreboot/keepassxc/releases/download/2.7.6
-set PROG=KeePassXC-2.7.6-Win64.msi
+set VERSION=2.7.7
+set BASEURL=https://github.com/keepassxreboot/keepassxc/releases/download/%VERSION%
+set PROG=KeePassXC-%VERSION%-Win64.msi
+
 rem --- Lade Installationsdatei herunter
 curl -k -L %BASEURL%/%PROG% -o %SAVEPATH%\%PROG%
+
 rem --- Silent-Installation des Programms:
 %SYSTEMDRIVE%\windows\system32\msiexec /i "%PROG%" /passive /l %SAVEPATH%\%PROG%.log 
-
-
-
-
-
-
 
